b0e74cd180d9d113a62184ef5d6129660415af7c
iamageo <geovani.af4@gmail.com>
2020-12-17 10:47:10 -0300

    finalizando header e adicionando função javascript para menu

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+/* mostrar menu */
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/* ativar e remover menu*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    /* ativar link */
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    /* remover menu mobile */
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
